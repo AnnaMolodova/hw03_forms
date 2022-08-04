add5825d7c31d6e5b17fef0369ded454aa41d48d
Anna Molodova <hyperclover@gmail.com>
2022-08-04 13:42:24 +0300

    Можно создавать посты

---
+++ yatube/posts/views.py
@@ -0,0 +1,96 @@
+from django.core.paginator import Paginator
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+
+from .models import Group, Post, User
+from .forms import PostForm
+
+
+User = get_user_model()
+
+
+def index(request):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    template = 'posts/profile.html'
+    author = get_object_or_404(User, username=username)
+    posts = author.posts.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'page_obj': page_obj,
+    }
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+    template = 'posts/post_detail.html'
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'post': post
+    }
+    return render(request, template, context)
+
+
+@login_required
+def post_create(request):
+    template = 'posts/create_post.html'
+    form = PostForm(
+        request.POST or None,
+        files=request.FILES or None
+    )
+    if form.is_valid():
+        temp_form = form.save(commit=False)
+        temp_form.author = request.user
+        temp_form.save()
+        return redirect(
+            'posts:profile', temp_form.author
+        )
+    return render(request, template, {'form': form})
+
+
+def post_edit(request, post_id):
+    template = 'posts/create_post.html'
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect(
+            'posts:post_detail', post_id
+        )
+    form = PostForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=post
+    )
+    if form.is_valid():
+        form.save()
+        return redirect(
+            'posts:post_detail', post_id
+        )
+    return render(request, template, {
+        'form': form, 'is_edit': True, 'post': post
+    })
